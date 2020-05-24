8cb56817fe74e4c3fa26668826b56f1f5d4144ba
Serg Nazarchuk <tesejz@gmail.com>
2020-05-24 23:20:13 +0300

    Updates

---
+++ precache-manifest.81f43191d1f75272424491d8792048c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d8aa69fa2c5814a73e9c6ec01baad0",
+    "url": "/react-Todolist/index.html"
+  },
+  {
+    "revision": "9dffc4079ec317d42b88",
+    "url": "/react-Todolist/static/css/main.23b58a7c.chunk.css"
+  },
+  {
+    "revision": "fbfd4dbdc6b1c3c669c2",
+    "url": "/react-Todolist/static/js/2.e1987291.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/react-Todolist/static/js/2.e1987291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dffc4079ec317d42b88",
+    "url": "/react-Todolist/static/js/main.9815eac7.chunk.js"
+  },
+  {
+    "revision": "db915f71aafd415de384",
+    "url": "/react-Todolist/static/js/runtime-main.1117fa7e.js"
+  }
+]);
